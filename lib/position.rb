a787891615498b32e75e20b96a8a2a87c50dc3e5
Prince <prince@mobomo.com>
2017-01-04 01:35:49 +0530

    Making x_range and y_range private methods.

--- lib/position.rb
+++ lib/position.rb
@@ -1,15 +1,17 @@
 require_relative 'constants'
 
 class Position < Struct.new(:x, :y)
+  def valid?
+    x_range.include?(x) && y_range.include?(y)
+  end
+
+  private
+
   def x_range
     (0...TABLE_DIMENSIONS[:length])
   end
 
   def y_range
     (0...TABLE_DIMENSIONS[:breadth])
-  end
-
-  def valid?
-    x_range.include?(x) && y_range.include?(y)
   end
 end
